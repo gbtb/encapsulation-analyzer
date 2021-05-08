ecb4f2b8b74ce84a1ee3bc4a602f443695b5abe6
utin.as <utin.as@dns-shop.ru>
2021-05-08 11:49:55 +1000

    make things actually work and wrote first test

--- EncapsulationAnalyzer.CLI/Program.cs
+++ EncapsulationAnalyzer.CLI/Program.cs
@@ -4,8 +4,6 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Buildalyzer;
-using Buildalyzer.Workspaces;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.FindSymbols;
@@ -18,11 +16,6 @@ namespace EncapsulationAnalyzer.CLI
         static Program()
         {
             MSBuildLocator.RegisterDefaults();
-            //var instances = MSBuildLocator.QueryVisualStudioInstances().ToList();
-            //MSBuildLocator.RegisterInstance(instances.First());
-            //MSBuildLocator.RegisterMSBuildPath(@"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin");
-            //MSBuildLocator.RegisterMSBuildPath(@"C:\Program Files\dotnet\sdk\5.0.101\");
-            //MSBuildLocator.RegisterMSBuildPath(@"C:\Program Files\dotnet\sdk\2.1.202");
         }
         
         static async Task Main(string[] args)
@@ -65,83 +58,6 @@ namespace EncapsulationAnalyzer.CLI
         private static void HandleWorkspaceFailure(object sender, WorkspaceDiagnosticEventArgs e)
         {
             Console.Error.WriteLine(e.Diagnostic.ToString());
-        }
-
-        private static ImmutableHashSet<Document> GetDocsToSearchIn(Solution solution, Project proj)
-        {
-            var graph = solution.GetProjectDependencyGraph();
-            var otherProjects = graph.GetProjectsThatDirectlyDependOnThisProject(proj.Id);
-            return otherProjects.SelectMany(id => solution.GetProject(id).Documents).ToImmutableHashSet();
-        }
-
-        private static IEnumerable<INamedTypeSymbol> GetNamedTypeSymbols(Compilation compilation,
-            IAssemblySymbol compilationAssembly, Predicate<INamedTypeSymbol> searchPredicate)
-        {
-            var stack = new Stack<INamespaceSymbol>();
-            stack.Push(compilation.GlobalNamespace);
-
-            while (stack.Count > 0)
-            {
-                var @namespace = stack.Pop();
-
-                foreach (var member in @namespace.GetMembers())
-                {
-                    if (member.ContainingAssembly?.Equals(compilationAssembly) == false)
-                        continue;
-                    
-                    if (member is INamespaceSymbol memberAsNamespace)
-                    {
-                        stack.Push(memberAsNamespace);
-                    }
-                    else if (member is INamedTypeSymbol memberAsNamedTypeSymbol)
-                    {
-                        if (searchPredicate(memberAsNamedTypeSymbol))
-                            yield return memberAsNamedTypeSymbol;
-                    }
-                }
-            }
-        }
-    }
-
-    internal class FindRefProgress : IFindReferencesProgress
-    {
-        private readonly CancellationTokenSource _source;
-        private readonly Project _project;
-
-        internal FindRefProgress(CancellationTokenSource source, Project project)
-        {
-            _source = source;
-            _project = project;
-        }
-        
-        public void OnStarted()
-        {
-        }
-
-        public void OnCompleted()
-        {
-        }
-
-        public void OnFindInDocumentStarted(Document document)
-        {
-        }
-
-        public void OnFindInDocumentCompleted(Document document)
-        {
-        }
-
-        public void OnDefinitionFound(ISymbol symbol)
-        {
-        }
-
-        public void OnReferenceFound(ISymbol symbol, ReferenceLocation location)
-        {
-            if (location.Document.Project != _project)
-                _source.Cancel();
-        }
-
-        public void ReportProgress(int current, int maximum)
-        {
         }
     }
 }
