c7c3945bd498e813490ffdfe556ef339a822284a
utin.as <utin.as@dns-shop.ru>
2021-05-09 17:04:04 +1000

    now logic also considers InternalsVIsibleTo attribute

--- EncapsulationAnalyzer/EncapsulationAnalyzer.Test/EncapsulationAnalyzerUnitTests.cs
+++ EncapsulationAnalyzer/EncapsulationAnalyzer.Test/EncapsulationAnalyzerUnitTests.cs
@@ -12,6 +12,7 @@ using Assert = NUnit.Framework.Assert;
 namespace EncapsulationAnalyzer.Test
 {
     [TestFixture]
+    [Parallelizable(ParallelScope.All)]
     public class EncapsulationAnalyzerUnitTest
     {
         private ServiceProvider _provider;
@@ -71,10 +72,66 @@ namespace EncapsulationAnalyzer.Test
 
             var service = _provider.GetRequiredService<IFindInternalClassesPort>();
             var source = new CancellationTokenSource();
-            //source.CancelAfter(TimeSpan.FromSeconds(10));
             var internalSymbols = await service.FindProjClassesWhichCanBeInternalAsync(workspace.CurrentSolution, libProject.Id, new Progress<FindInternalClassesProgress>(), source.Token);
             Assert.AreEqual(1, internalSymbols.Count());
             Assert.AreEqual("Bar", internalSymbols.Single().Name);
+        }
+        
+        [Test]
+        public async Task TwoSimpleProjectsTwoSimpleClasses_InternalsVisibleToTest()
+        {
+            var workspace = new AdhocWorkspace();
+            var solution = workspace.AddSolution(SolutionInfo.Create(SolutionId.CreateNewId(), VersionStamp.Default));
+            var libProject = workspace.AddProject(ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Default, "Lib", "Lib", LanguageNames.CSharp));
+
+            var sourceText = SourceText.From(@"
+                using System;
+                using System.Runtime.CompilerServices;
+                
+                [assembly: InternalsVisibleTo(""UI"")]
+                namespace Lib 
+                {
+                    public class Foo 
+                    {
+                        public int Prop { get; set; }
+                    }
+
+                    public class Bar 
+                    {
+                       public int Prop { get; set; }
+                    }
+                }
+            ");
+            workspace.AddDocument(libProject.Id, "Lib.cs", sourceText);
+            var uiProject = workspace.AddProject(ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Default, "UI", "UI", LanguageNames.CSharp, 
+                    projectReferences: new [] {new ProjectReference(libProject.Id)}));
+            sourceText = SourceText.From(@"
+                using System;
+                using Lib;
+
+                namespace UI
+                {
+                    public static class Program 
+                    {
+                        public static void Main(string[] args)
+                        {
+                            var foo = new Foo();
+                            foo.Prop = 1;
+                        }
+                    }
+                }
+            ");
+            workspace.AddDocument(uiProject.Id, "UI.cs", sourceText);
+
+
+            var service = _provider.GetRequiredService<IFindInternalClassesPort>();
+            var source = new CancellationTokenSource();
+            var internalSymbols = await service.FindProjClassesWhichCanBeInternalAsync(workspace.CurrentSolution, libProject.Id, new Progress<FindInternalClassesProgress>(), source.Token);
+            Assert.AreEqual(2, internalSymbols.Count());
+            
+            
+            Assert.AreEqual("Bar", internalSymbols.OrderBy(s => s.Name).First().Name);
+            Assert.AreEqual("Foo", internalSymbols.OrderBy(s => s.Name).Last().Name);
         }
         
     }
