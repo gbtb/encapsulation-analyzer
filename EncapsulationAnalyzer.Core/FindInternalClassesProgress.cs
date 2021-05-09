8ef9dfefe7ed500da75a9dac490ad3c7ebf71c13
utin.as <utin.as@dns-shop.ru>
2021-05-09 12:37:11 +1000

    messing with logging and progress display - they are not compatible as is

--- EncapsulationAnalyzer.Core/FindInternalClassesProgress.cs
+++ EncapsulationAnalyzer.Core/FindInternalClassesProgress.cs
@@ -2,11 +2,18 @@
 {
     public record FindInternalClassesProgress
     {
-        public FindInternalClassesStep Step { get; init; }
+        public FindInternalClassesProgress(FindInternalClassesStep step, int? currentValue, int? maxValue = null)
+        {
+            Step = step;
+            CurrentValue = currentValue;
+            MaxValue = maxValue;
+        }
+
+        public FindInternalClassesStep Step { get; }
         
-        public int? CurrentValue { get; init; }
+        public int? CurrentValue { get; }
         
-        public int? MaxValue { get; init; }
+        public int? MaxValue { get; }
     }
 }
 
